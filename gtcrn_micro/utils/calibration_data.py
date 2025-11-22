d2728ed8bea27ad4bd1c2c1c544eb858172dc966
bglid <benjamin.h.glidden@gmail.com>
2025-11-21 19:22:24 -0500

    Adjusting pre-architecture change

---
+++ gtcrn_micro/utils/calibration_data.py
@@ -0,0 +1,77 @@
+# used for onnx2tf.sh
+# ----------------
+import numpy as np
+import soundfile as sf
+import torch
+from pathlib import Path
+
+# starting with basic data calibration
+# NOTE: Need to improve in the future
+CALIB_DATA = Path(
+    "./gtcrn_micro/data/DNS3/V2_V3_DNSChallenge_Blindset/noisy_blind_testset_v3_challenge_withSNR_16k/"
+)
+OUTPUT = Path("./gtcrn_micro/models/tflite/tflite_calibration.npy")
+
+# CONSTANTS FOR STFT INFO
+N_FFT = 512
+HOP = 256
+
+
+# function to generate stft tensors
+def wav_2_tensor(wav_path: Path):
+    mix, fs = sf.read(
+        str(wav_path),
+        dtype="float32",
+    )
+
+    assert fs == 16000, f"Expected 16kHz, got {fs}"
+
+    stft = torch.stft(
+        torch.from_numpy(mix),
+        N_FFT,
+        HOP,
+        N_FFT,
+        torch.hann_window(N_FFT).pow(0.5),
+        return_complex=False,
+    )
+
+    # specifically for the tf model conversion we need to transpose the inputs
+    stft_np = stft.numpy().transpose(1, 2, 0)
+    return stft_np
+
+
+def main():
+    # getting .wav files in directory
+    # only taking 32 samples for now
+    wavs = sorted(CALIB_DATA.glob("*.wav"))[:32]
+    data = []
+    # appending the tensor stft to data list
+    for i in wavs:
+        stft_np = wav_2_tensor(i)
+        data.append(stft_np)
+
+    # need to pad or truncate data
+    # max frames comes from previously working with conversion
+    max_frames = 973
+    padding = []
+    # getting every tensor and checking it's shapes
+    for tsr in data:
+        T, C, F = tsr.shape
+        if T >= max_frames:
+            tsr = tsr[:max_frames]
+        # else do padding
+        else:
+            pad = np.zeros((max_frames - T, C, F), dtype=tsr.dtype)
+            tsr = np.concatenate([tsr, pad], axis=0)
+
+        padding.append(tsr)
+
+    # reconstructing the now padded data (if needed)
+    data = np.stack(padding, axis=0)
+
+    print(f"**********\nData shape: {data.shape}\n**********")
+    np.save(OUTPUT, data.astype("float32"))
+
+
+if __name__ == "__main__":
+    main()
--- scripts/onnx2tf.sh
+++ scripts/onnx2tf.sh
@@ -7,6 +7,14 @@ ONNX_INPUT="gtcrn_micro/models/onnx/"
 ONNX_FILE=gtcrn_micro.onnx # testing lowered opset 16
 OUTPUT_PATH="gtcrn_micro/models/tflite/"
 JSON_FILE=replace_gtcrn_micro.json
+CALIB_DATA="${OUTPUT_PATH}tflite_calibration.npy"
+
+# double check file exists
+if [ -e "$CALIB_DATA" ]; then
+	echo "$CALIB_DATA exists."
+else
+	echo "$CALIB_DATA does not exist"
+fi
 
 # run onnx conversion
 uv run onnx2tf \
@@ -17,4 +25,5 @@ uv run onnx2tf \
 	-prf ${OUTPUT_PATH}${JSON_FILE} \
 	-cotof \
 	-oiqt \
-	-qt per-channel
+	-qt per-channel \
+	-cind "audio" "$CALIB_DATA" "[[[[0.]]]]" "[[[[1.]]]]"
