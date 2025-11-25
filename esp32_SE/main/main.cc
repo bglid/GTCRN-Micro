c1fd993b6eea99bc5de96aee6e26104d75e322ab
bglid <benjamin.h.glidden@gmail.com>
2025-11-25 15:41:39 -0500

    updating main.cc beta test to a version that can flash to ESP32-S3

--- esp32_SE/main/main.cc
+++ esp32_SE/main/main.cc
@@ -1,20 +1,28 @@
+/*
+NOTE: All of this is for running tests and should be cleaned up
+*/
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 
 #include "driver/gpio.h"
+#include "esp_attr.h"
 #include "esp_log.h"
 #include "esp_system.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #include "gtcrn_micro_int8_data.cc"
 
-#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/core/c/common.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_log.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "tensorflow/lite/version.h"
 
+#define TFLITE_SCHEMA_VERSION (3)
 // logger for esp32
 static const char *TAG = "GTCRN_MICRO_TEST";
 
@@ -22,30 +30,79 @@ static const char *TAG = "GTCRN_MICRO_TEST";
 static const gpio_num_t led_pin = GPIO_NUM_38;
 static const int32_t sleep_time_ms = 200;
 
-static constexpr int kTensorArenaSize = 400 * 1024;
-static uint8_t tensor_arena[kTensorArenaSize];
+// allocate Tensor Arena size on PSRAM
+constexpr int kTensorArenaSize = 7500 * 1024;
+uint8_t *tensor_arena = nullptr;
+
+// logging
+void error_blink(const char *msg) {
+  while (1) {
+    printf("ERROR: %s\n", msg);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
 
 extern "C" void app_main(void) {
-  int8_t led_state = 0;
-
-  // config GPIO
-  gpio_reset_pin(led_pin);
-  gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
 
   ESP_LOGI(TAG, "Starting gtcrn_micro test");
+  ESP_LOGI(TAG, "Free internal heap: %d", (int)esp_get_free_heap_size());
+  ESP_LOGI(TAG, "Free SPIRAM: %d",
+           (int)heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
+
+  // heap allocate tensor arena for debugging
+  tensor_arena = (uint8_t *)heap_caps_malloc(
+      kTensorArenaSize, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
+
+  // debug malloc
+  if (!tensor_arena) {
+    ESP_LOGE(TAG, "Failed to allocate Tensor in PSRAM");
+    error_blink("PSRAM Arena malloc failed");
+    return;
+  }
+
+  // worked?
+  ESP_LOGI(TAG, "Arena allocated at %p, size %d", tensor_arena,
+           kTensorArenaSize);
+
+  // log remaining memory
+  ESP_LOGI(TAG, "Remaining internal heap %d", (int)esp_get_free_heap_size());
+  ESP_LOGI(TAG, "Remaining SPIRAM: %d",
+           (int)heap_caps_get_free_size(MALLOC_CAP_SPIRAM));
 
   // SETTING UP TF
-  const tflite::Model *model = tflite::GetModel(gtcrn_micro_int8_tflite);
+  const tflite::Model *model =
+      tflite::GetModel(gtcrn_micro_full_integer_quant_tflite);
   if (model->version() != TFLITE_SCHEMA_VERSION) {
-    ESP_LOGE(TAG, "Model schema %d not equal to supported %d", model->version(),
-             TFLITE_SCHEMA_VERSION);
+    ESP_LOGE(TAG, "Model schema %" PRIu32 "not equal to supported %" PRIu32,
+             static_cast<u_int32_t>(model->version()),
+             static_cast<u_int32_t>(TFLITE_SCHEMA_VERSION));
+    error_blink("Schema mismatch");
     return;
   }
 
   // resolve ops
-  /*static tflite::AllOpsResolver resolver;*/
-  // Switching to micro_ops
-  /*static tflite::MicroMutableOpResolver*/
+  // static tflite::AllOpsResolver resolver;
+  static tflite::MicroMutableOpResolver<24> resolver;
+  // registering the ops
+  resolver.AddGather();
+  resolver.AddMul();
+  resolver.AddAdd();
+  resolver.AddTranspose();
+  resolver.AddReshape();
+  resolver.AddDequantize();
+  resolver.AddSqrt();
+  resolver.AddQuantize();
+  resolver.AddConcatenation();
+  resolver.AddStridedSlice();
+  resolver.AddFullyConnected();
+  resolver.AddConv2D();
+  resolver.AddAbs();
+  resolver.AddSub();
+  resolver.AddRelu();
+  resolver.AddPad();
+  resolver.AddDepthwiseConv2D();
+  resolver.AddTransposeConv();
+  resolver.AddTanh();
 
   // setup interpreter
   static tflite::MicroInterpreter interpreter(model, resolver, tensor_arena,
@@ -55,6 +112,7 @@ extern "C" void app_main(void) {
   TfLiteStatus alloc_status = interpreter.AllocateTensors();
   if (alloc_status != kTfLiteOk) {
     ESP_LOGE(TAG, "AllocateTensors() failed");
+    error_blink("Alloc failed");
     return;
   }
 
@@ -71,7 +129,7 @@ extern "C" void app_main(void) {
       total *= input->dims->data[i];
     }
     for (int i = 0; i < total; ++i) {
-      data[i] = 0; // silence / zero STFT
+      data[i] = 0;
     }
   } else if (input->type == kTfLiteFloat32) {
     float *data = input->data.f;
@@ -87,15 +145,26 @@ extern "C" void app_main(void) {
     return;
   }
 
-  // 5) Invoke once
+  // Invoke once
+  // do a timer check
+  ESP_LOGI(TAG, "Invoking model...");
+  int64_t t0 = esp_timer_get_time();
   TfLiteStatus invoke_status = interpreter.Invoke();
+  int64_t t1 = esp_timer_get_time();
+
   if (invoke_status != kTfLiteOk) {
     ESP_LOGE(TAG, "Invoke() failed");
     return;
+  } else {
+    ESP_LOGI(TAG, "Invoke time: %lld us", (long long)(t1 - t0));
   }
 
   ESP_LOGI(TAG, "Inference succeeded – blinking LED");
 
+  int8_t led_state = 0;
+  // config GPIO
+  gpio_reset_pin(led_pin);
+  gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
   // loop and blink LED if nothing else failed!
   while (1) {
     // toggle the LED
